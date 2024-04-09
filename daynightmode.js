72d4c27b537968c95fe15277ff213e59db9a5e99
Faith Best <97316553+ibringfaith@users.noreply.github.com>
2024-04-09 14:08:52 -0400

    Add files via upload

--- daynightmode.js
+++ daynightmode.js
@@ -6,18 +6,25 @@ function updateTimeStatus() {
 
   if (hour >= 0 && hour < 8) {
     status = "breakone";
-  } else if (hour >=9 && hour <= 10) {
+    document.body.style.backgroundColor = '#685E5A';
+  } else if (hour >=9 && hour < 10) {
     status = "breaktwo";
-  } else if (hour >= 12 && hour <= 13) {
+    document.body.style.backgroundColor = '#827570';
+  } else if (hour >= 12 && hour < 13) {
     status = "breakthree";
-  } else if (hour >= 17 && hour <= 18) {
+    document.body.style.backgroundColor = '#827570';
+  } else if (hour >= 17 && hour < 18) {
     status = "breakfour";
-  } else if (hour >= 21 && hour <= 22) {
+    document.body.style.backgroundColor = '#685E5A';
+  } else if (hour >= 21 && hour < 22) {
     status = "breakfive";
-  } else if (hour >= 5 && hour <= 17) {
+    document.body.style.backgroundColor = '#685E5A';
+  } else if (hour >= 5 && hour < 17) {
     status = "light";
+    document.body.style.backgroundColor = '#827570';
   } else {
     status = "dark";
+    document.body.style.backgroundColor = '#685E5A';
   }
 
   document.body.setAttribute("data-status", status);
